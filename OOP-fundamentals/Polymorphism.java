5505f9f39dc3b1a74968de882168df2fdc4ed6ce
mahesh <umamahesh.ch1995@gmail.com>
2026-03-14 15:00:17 -0400

    practice

--- OOP-fundamentals/Polymorphism.java
+++ OOP-fundamentals/Polymorphism.java
@@ -1,28 +1,45 @@
 //Compile Time Polymorphism - Method Overloading
 
-class Calculator {
-    // Two ints
-    int add(int a, int b) {
-        return a + b;
-    }
+// class Calculator {
+//     // Two ints
+//     int add(int a, int b) {
+//         return a + b;
+//     }
 
-    // Two doubles
-    double add(double a, double b) {
-        return a + b;
-    }
+//     // Two doubles
+//     double add(double a, double b) {
+//         return a + b;
+//     }
 
-    // Three ints
-    int add(int a, int b, int c) {
-        return a + b + c;
-    }
+//     // Three ints
+//     int add(int a, int b, int c) {
+//         return a + b + c;
+//     }
+// }
+
+// public class Polymorphism {
+//     public static void main(String[] args) {
+//         Calculator calc = new Calculator();
+//         System.out.println(calc.add(2, 3));        // Calls add(int, int) -> 5
+//         System.out.println(calc.add(2.5, 3.5));    // Calls add(double, double) -> 6.0
+//         System.out.println(calc.add(1, 2, 3));     // Calls add(int, int, int) -> 6
+//     }
+// }
+//------------------------------------------------------------------------------------------
+
+//Compile-time Polymorphism(Method Overloading) -> depends before program runs
+class Calculator{
+    int add(int a, int b){return a+b;}
+    double add(double a, double b){return a+b;}
+    int add(int a, int b, int c){return a+b+c;}
 }
-
-public class Polymorphism {
+public class Polymorphism{
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(2, 3));        // Calls add(int, int) -> 5
-        System.out.println(calc.add(2.5, 3.5));    // Calls add(double, double) -> 6.0
-        System.out.println(calc.add(1, 2, 3));     // Calls add(int, int, int) -> 6
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(2,3));
+        System.out.println(cal.add(2.3,6.4));
+        System.out.println(cal.add(1,2,3));
+
     }
 }
 
