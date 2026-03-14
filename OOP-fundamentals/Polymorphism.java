cb04cd00702b6c0bc756862453b8a24a770d27f1
mahesh <umamahesh.ch1995@gmail.com>
2026-03-14 15:16:05 -0400

    practice

--- OOP-fundamentals/Polymorphism.java
+++ OOP-fundamentals/Polymorphism.java
@@ -28,19 +28,67 @@
 //------------------------------------------------------------------------------------------
 
 //Compile-time Polymorphism(Method Overloading) -> depends before program runs
-class Calculator{
-    int add(int a, int b){return a+b;}
-    double add(double a, double b){return a+b;}
-    int add(int a, int b, int c){return a+b+c;}
+// class Calculator{
+//     int add(int a, int b){return a+b;}
+//     double add(double a, double b){return a+b;}
+//     int add(int a, int b, int c){return a+b+c;}
+// }
+// public class Polymorphism{
+//     public static void main(String[] args) {
+//         Calculator cal = new Calculator();
+//         System.out.println(cal.add(2,3));
+//         System.out.println(cal.add(2.3,6.4));
+//         System.out.println(cal.add(1,2,3));
+
+//     }
+// }
+//------------------------------------------------------------------------------------------
+
+//Run-Time Polymorphism(Method overriding/dynamic dispatch) -> depends while program runs
+
+import java.util.List;
+
+class Notification{
+    protected String recipient;
+    protected String message;
+    public Notification(String recipient, String message){
+        this.recipient = recipient;
+        this.message = message;
+    }
+    public void send(){
+        System.out.println("Sending generic notification to "+recipient);
+    }
+}
+class EmailNotification extends Notification{
+    private String subject;
+    public EmailNotification(String recipient, String message, String subject){
+        super(recipient, message);
+        this.subject = subject;
+    }
+    @Override
+    public void send(){
+        System.out.println("Sending Email to "+recipient+ " | subject: "+subject);
+    }
+}
+class SMSNotification extends Notification{
+    private String phoneNumber;
+    public SMSNotification(String recipient, String message, String phoneNumber){
+        super(recipient, message);
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public void send(){
+        System.out.println("Sending SMS to "+phoneNumber+" | Message: "+message);
+    }
 }
 public class Polymorphism{
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.add(2,3));
-        System.out.println(cal.add(2.3,6.4));
-        System.out.println(cal.add(1,2,3));
-
+        List<Notification> notifications = List.of(
+            new EmailNotification("mahesh@gmail.com", "Completed", "RunTime Polymorphism"),
+            new SMSNotification("mahesh", "through SMS", "123456789")
+        );
+        for(Notification n : notifications){
+            n.send();
+        }
     }
 }
-
-
