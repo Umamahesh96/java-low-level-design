fa653c2a28f4ecf4920e45cb3ed6b06a0ee185ca
mahesh <umamahesh.ch1995@gmail.com>
2026-03-22 10:21:58 -0400

    practice

--- Class-Relationships/Realization.java
+++ Class-Relationships/Realization.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 interface Flyable{
     void fly();
     String getFlightInfo();
@@ -17,8 +19,31 @@ class Bird implements Flyable{
         return species + " (wingspan: "+wingSpan+"m, get powered by muscle)";
     }
 }
+class Airplane implements Flyable{
+    private String model;
+    private int maxAltitude;
+    public Airplane(String model, int maxAltitude){
+        this.model = model;
+        this.maxAltitude = maxAltitude;
+    }
+    public void fly(){
+        System.out.println(model+" engines roar as it accelerates down the runway");
+    }
+    public String getFlightInfo(){
+        return model + " (max Altitude : "+maxAltitude+" powered by jet engines";
+    }
+}
 
 
 public class Realization {
-    
+    public static void main(String[] args) {
+        List<Flyable> flyingThings = new ArrayList<>();
+        flyingThings.add(new Bird("Eagle", 2.3));
+        flyingThings.add(new Airplane("Boeing 737", 41000));
+        for(Flyable things : flyingThings){
+            System.out.println(things.getFlightInfo());
+            things.fly();
+            System.out.println();
+        }
+    }
 }
